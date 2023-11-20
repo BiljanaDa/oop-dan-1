9a0a3d371ef8003b016a6ba0131f633724881ee4
BiljanaDa <bibidakovic@gmail.com>
2023-11-20 19:18:32 +0100

    vezba1

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+class Users {
+    private $username;
+    private $email;
+    private $password;
+    
+
+   function __construct($username, $email, $password)
+   {
+    $this->username = $username;
+    $this->email = $email;
+    $this->password = $password;
+   }
+
+   public function getPassword():string {
+    return $this->password;
+   }
+
+   public function setPassword(string $password): void {
+    $this->password = $password;
+   }
+
+   public function getUsername():string {
+    return $this->username;
+   }
+
+   public function setUsername(string $username): void {
+    $this->username = $username;
+   }
+
+   public function getEmail():string {
+    return $this->email;
+   }
+
+   public function setEmail(string $email): void {
+    $this->email = $email;
+   }
+}
+
+$pera = new Users("Pera", "pera@mail.com", "12345");
+$mika = new Users("Mika", "mika@mail", "9876");
+$zika = new Users("Zika", "zika@mail", "5678");
+
+print_r($pera);
+print_r($mika);
+print_r($zika);
